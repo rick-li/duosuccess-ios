f63fecab02d60ddee5a09dc219b8bd65133d3c40
Rick Li <racke1983cn@gmail.com>
2013-12-10 19:44:05 +0800

    Initial Commit

---
+++ duosuccess/DsAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  DsAppDelegate.h
+//  duosuccess
+//
+//  Created by Rick Li on 12/10/13.
+//  Copyright (c) 2013 Rick Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ duosuccess/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  duosuccess
+//
+//  Created by Rick Li on 12/10/13.
+//  Copyright (c) 2013 Rick Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DsAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DsAppDelegate class]));
+    }
+}
