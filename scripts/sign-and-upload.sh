72118bfca102c63fd39b98ecda63918756783827
Rick Li <racke1983cn@gmail.com>
2014-01-07 23:41:00 +0800

    use firapp for beta release

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -19,16 +19,25 @@ zip -r -9 "$OUTPUTDIR/$APP_NAME.app.dSYM.zip" "$OUTPUTDIR/$APP_NAME.app.dSYM"
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
-if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
-  echo ""
-  echo "***************************"
-  echo "* Uploading to Testflight *"
-  echo "***************************"
-  curl http://testflightapp.com/api/builds.json \
-    -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
-    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
-    -F api_token="$TESTFLIGHT_API_TOKEN" \
-    -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
-    -F distribution_lists='Internal' \
-    -F notes="$RELEASE_NOTES"
-fi
+#use firapp instead
+curl http://bcs.duapp.com/apphost1/com.duosuccess.duosuccess.ipa?sign=MBO:08fadd5a7e397b10f4599c325ee55b9c:2tRiqUpGjY2lQP8zTnZEoATSE6k%3D \
+    -T "@$OUTPUTDIR/$APP_NAME.ipa" 
+
+curl http://firapp.duapp.com/api/finish \
+    -F appid="com.duosuccess.duosuccess" \
+    -F short="8eF" \
+    -F version="Build $TRAVIS_BUILD_NUMBER $RELEASE_DATE"
+
+#if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
+#  echo ""
+#  echo "***************************"
+#  echo "* Uploading to Testflight *"
+#  echo "***************************"
+#  curl http://testflightapp.com/api/builds.json \
+#    -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+#    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+#    -F api_token="$TESTFLIGHT_API_TOKEN" \
+#    -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
+#    -F distribution_lists='Internal' \
+#    -F notes="$RELEASE_NOTES"
+#fi
