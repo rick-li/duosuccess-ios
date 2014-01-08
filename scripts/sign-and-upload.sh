d73c1ca897fcd4e2932340d4b484feb9dee3900e
Rick Li <racke1983cn@gmail.com>
2014-01-08 16:52:13 +0800

    also enable testflight

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -28,16 +28,16 @@ curl http://firapp.duapp.com/api/finish \
     -F short="8eF" \
     -F version="Build $TRAVIS_BUILD_NUMBER $RELEASE_DATE"
 
-#if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
-#  echo ""
-#  echo "***************************"
-#  echo "* Uploading to Testflight *"
-#  echo "***************************"
-#  curl http://testflightapp.com/api/builds.json \
-#    -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
-#    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
-#    -F api_token="$TESTFLIGHT_API_TOKEN" \
-#    -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
-#    -F distribution_lists='Internal' \
-#    -F notes="$RELEASE_NOTES"
-#fi
+if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
+  echo ""
+  echo "***************************"
+  echo "* Uploading to Testflight *"
+  echo "***************************"
+  curl http://testflightapp.com/api/builds.json \
+    -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+    -F api_token="$TESTFLIGHT_API_TOKEN" \
+    -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
+    -F distribution_lists='Internal' \
+    -F notes="$RELEASE_NOTES"
+fi
